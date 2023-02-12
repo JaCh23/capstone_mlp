5a6c291d2dc90b80f2026cc07fd070b2ab889ee6
jared <jach2355@gmail.com>
2023-02-12 15:29:25 +0800

    mlp cpp tidied

--- mlp.cpp
+++ mlp.cpp
@@ -4,10 +4,6 @@
 #include <algorithm>
 #include "read_file.cpp"
 
-// std::pair<int, int> get_dimensions(const std::vector<std::vector<int>>& array) {
-//     return {array.size(), array[0].size()};
-// }
-
 void print_vector(const std::vector<double>& v) {
     for (auto i : v) {
         std::cout << i << " ";
@@ -30,8 +26,6 @@ std::vector<double> sigmoid(const std::vector<double> &x) {
     }
     return result;
 }
-
-// 0.51349672045, 0.52398158497
 
 // Softmax activation function
 std::vector<double> softmax(const std::vector<double> &x) {
@@ -56,29 +50,15 @@ std::vector<double> compute_layer(const std::vector<double> &inputs, const std::
         double dot_product = 0.0;
         for(int j = 0; j < inputs.size(); j++)
         {
-            //multiplying input by weight
-            // std::cout << "multiplying input by weight: " << inputs[j] << " * " << weights[j][i] << std::endl;
             dot_product += inputs[j] * weights[j][i];
         }
         outputs[i] = dot_product;
-        // std::cout << outputs[i] << std::endl;
     }
     return outputs;
 }
 
-// std::vector<double> mlp(std::vector<double>&inputs)
 std::vector<double> mlp(std::vector<double>inputs)
 {
-    // std::vector<double> inputs = read_file_arr("input.txt");
-
-    // Assign the weights and biases from the header file
-    // std::vector<double> weights_1 = {0.1, 0.2, 0.3, 0.4};
-    // std::vector<double> biases_1 = {0.5, 0.6};
-    // std::vector<double> weights_2 = {0.7, 0.8, 0.9, 1.0};
-    // std::vector<double> biases_2 = {1.1, 1.2};
-    // std::vector<double> weights_3 = {1.3, 1.4, 1.5, 1.6};
-    // std::vector<double> biases_3 = {1.7, 1.8};
-
     std::vector<std::vector<double>> weights_1 = read_file("array_0.txt");
     std::vector<double> biases_1 = read_file_arr("array_1.txt");
     std::vector<std::vector<double>> weights_2 = read_file("array_2.txt");
@@ -86,54 +66,17 @@ std::vector<double> mlp(std::vector<double>inputs)
     std::vector<std::vector<double>> weights_3 = read_file("array_4.txt");
     std::vector<double> biases_3 = read_file_arr("array_5.txt");
 
-    
-   
-
-    // std::cout << "biases_1 size;" << biases_1.size() << std::endl;
-    // std::cout << "weights_2 size;" << weights_2.size() << std::endl;
-    // std::cout << "biases_2 size;" << biases_2.size() << std::endl;
-    // std::cout << "weights_3 size;" << weights_3.size() << std::endl;
-    // std::cout << "biases_3 size;" << biases_3.size() << std::endl;
-
-
-    // std::vector<double> hidden_layer_1 = compute_layer(inputs, weights_1, biases_1);
     std::vector<double> hidden_layer_1 = sigmoid(compute_layer(inputs, weights_1, biases_1));
-
     std::vector<double> hidden_layer_2 = sigmoid(compute_layer(hidden_layer_1, weights_2, biases_2));
     std::vector<double> outputs = softmax(compute_layer(hidden_layer_2, weights_3, biases_3));
 
-    // std::cout << "hidden layer 1:" ;
-    // print_vector(hidden_layer_1);
-
-    // std::cout << "sigmoid hidden layer 1:" ;
-    // print_vector(sigmoid(hidden_layer_1));
-    // print_vector(hidden_layer_2);
-    // print_vector(outputs);
-
-    // for(int i = 0; i < outputs.size(); i++)
-    // {
-    //     std::cout << outputs[i] << " ";
-    // }
-
-    // std::cout << std::endl;
-
     auto largest = std::max_element(outputs.begin(), outputs.end());
-
     int largest_classified_bucket = std::distance(outputs.begin(), largest);
 
     return oneHotEncode(largest_classified_bucket, outputs.size());
-
-    // return outputs; test
-    
-    // std::cout << "Output layer values: ";
-    // for(int i = 0; i < outputs.size(); ++i)
-    // {
-    //     std::cout << outputs[i] << " ";
-    // }
-    // std::cout << std::endl;
-    // return 0;
 }
 
+// Test function to check mlp function independently
 // int main() {
 //     std::vector<double> inputs = {0.36, 0.06};
 //     // std::vector<double> inputs = read_file_arr("test_input.txt");
--- mlp_test.cpp
+++ mlp_test.cpp
@@ -8,7 +8,6 @@
 
 using namespace std;
 
-
 vector<vector<double>> readData(const string& filename) {
     vector<vector<double>> data;
     ifstream file(filename);
@@ -47,40 +46,6 @@ void writeData(const vector<vector<double>>& data, const string& filename) {
     }
 }
 
-// bool compareData(const string& expectedFilename, const vector<vector<double>>& data) {
-//     ifstream expectedFile(expectedFilename);
-
-//     if (expectedFile.is_open()) {
-//         int rowNum = 0;
-//         string line;
-//         while (getline(expectedFile, line)) {
-//             stringstream ss(line);
-//             double expectedValue;
-//             int colNum = 0;
-//             while (ss >> expectedValue) {
-//                 double value = data[rowNum][colNum];
-//                 if (fabs(value - expectedValue) > 0.001) {
-//                     cout << "Test case failed at row " << rowNum << " column " << colNum << endl;
-//                     cout << "Expected value: " << expectedValue << ", but got: " << value << endl;
-//                     // return false;
-//                 }
-//                 else {
-//                     cout << "Test case passed at row " << rowNum << " column " << colNum << endl;
-//                     cout << "Expected value: " << expectedValue << ", got: " << value << endl;
-//                 }
-//                 colNum++;
-//             }
-//             rowNum++;
-//         }
-//         expectedFile.close();
-//     } else {
-//         cout << "Unable to open file " << expectedFilename << endl;
-//         return false;
-//     }
-
-//     return true;
-// }
-
 bool compareDataRow(const vector<double>& dataRow, const vector<double>& expectedDataRow) {
     bool flag = true;
     if (dataRow.size() != expectedDataRow.size()) {
@@ -93,25 +58,20 @@ bool compareDataRow(const vector<double>& dataRow, const vector<double>& expecte
         }
     }
 
+    cout << endl;
+
     if (flag) {
         cout << "Test case passed." << endl;
-        cout << "Expected vector: " ;
-        print_vector(expectedDataRow);
-
-        cout << "Actual vector: " ;
-        print_vector(dataRow);
-
     }
     else {
         cout << "Test case failed." << endl;
-        cout << "Expected vector: " ;
-        print_vector(expectedDataRow);
-
-        cout << "Actual vector: " ;
-        print_vector(dataRow);
-
     }
+    cout << "Expected vector: " ;
+    print_vector(expectedDataRow);
 
+    cout << "Actual vector: " ;
+    print_vector(dataRow);
+        
     return flag;
 }
 
@@ -121,8 +81,6 @@ int main() {
     vector<vector<double>> expectedData = readData("sim_labels.txt");
     vector<vector<double>> outputData;
     for (const vector<double>& row : inputData) {
-        // cout << "input row: " ;
-        // print_vector(row);
         outputData.push_back(mlp(row));
     }
     
@@ -147,6 +105,7 @@ int main() {
     }
 
     cout << "Number of test cases passed: " << count << endl;
+
     //accuracy of model
     cout << "Accuracy of model: " << (count*100)/outputData.size() << "%" << endl;
 
