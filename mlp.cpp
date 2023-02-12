0e0d3a914eed63b8f227b29048ebd2a8157bc1e5
jared <jach2355@gmail.com>
2023-02-12 20:40:43 +0800

    refactor mlp functions

--- mlp.cpp
+++ mlp.cpp
@@ -11,60 +11,12 @@
 
 std::vector<double> mlp(std::vector<double>inputs)
 {
-    auto sigmoid = [](const std::vector<double> &x) -> std::vector<double> 
-    {
-        std::vector<double> result(x.size());
-        for (int i = 0; i < x.size(); i++) 
-        {
-            result[i] = 1.0 / (1.0 + exp(-x[i]));
-        }
-        return result;
-    };
 
-    auto softmax = [](const std::vector<double> &x) -> std::vector<double>
-    {
-        std::vector<double> result(x.size());
-        double sum = 0.0;
-        for (int i = 0; i < x.size(); i++)
-        {
-            result[i] = exp(x[i]);
-            sum += result[i];
-        }
-        for (int i = 0; i < x.size(); i++)
-        {
-            result[i] /= sum;
-        }
-        return result;
-    };
-
-    auto oneHotEncode = [](int index, int size) {
-        std::vector<double> one_hot(size, 0);
-        one_hot[index] = 1.0;
-        return one_hot;
-    };
-
-    auto compute_layer = [](const std::vector<double> &inputs, const std::vector<std::vector<double>> &weights, const std::vector<double> &biases)
-    {
-        std::vector<double> outputs(biases.size());
-
-        for(int i = 0; i < outputs.size(); i++)
-        {
-            double dot_product = 0.0;
-            for(int j = 0; j < inputs.size(); j++)
-            {
-                dot_product += inputs[j] * weights[j][i];
-            }
-            outputs[i] = dot_product;
-        }
-        return outputs;
-    };
-
-
-    std::vector<std::vector<double>> weights_1 = read_file("array_0.txt");
+    std::vector<std::vector<double> > weights_1 = read_file("array_0.txt");
     std::vector<double> biases_1 = read_file_arr("array_1.txt");
-    std::vector<std::vector<double>> weights_2 = read_file("array_2.txt");
+    std::vector<std::vector<double> > weights_2 = read_file("array_2.txt");
     std::vector<double> biases_2 = read_file_arr("array_3.txt");
-    std::vector<std::vector<double>> weights_3 = read_file("array_4.txt");
+    std::vector<std::vector<double> > weights_3 = read_file("array_4.txt");
     std::vector<double> biases_3 = read_file_arr("array_5.txt");
 
     std::vector<double> hidden_layer_1 = sigmoid(compute_layer(inputs, weights_1, biases_1));
@@ -96,29 +48,84 @@ std::vector<double> mlp(std::vector<double>inputs)
 // }
 
 // Sigmoid activation function
-// std::vector<double> sigmoid(const std::vector<double> &x) {
+std::vector<double> sigmoid(const std::vector<double> &x) {
+    std::vector<double> result(x.size());
+    for (int i = 0; i < x.size(); i++) {
+        result[i] = 1.0 / (1.0 + exp(-x[i]));
+    }
+    return result;
+}
+
+// Softmax activation function
+std::vector<double> softmax(const std::vector<double> &x) {
+    std::vector<double> result(x.size());
+    double sum = 0.0;
+    for (int i = 0; i < x.size(); i++) {
+        result[i] = exp(x[i]);
+        sum += result[i];
+    }
+    for (int i = 0; i < x.size(); i++) {
+        result[i] /= sum;
+    }
+    return result;
+}
+
+std::vector<double> compute_layer(const std::vector<double> &inputs, const std::vector<std::vector<double>> &weights, const std::vector<double> &biases)
+{
+    std::vector<double> outputs(biases.size());
+
+    for(int i = 0; i < outputs.size(); i++)
+    {
+        double dot_product = 0.0;
+        for(int j = 0; j < inputs.size(); j++)
+        {
+            dot_product += inputs[j] * weights[j][i];
+        }
+        outputs[i] = dot_product;
+    }
+    return outputs;
+}
+
+// One hot encoding function
+std::vector<double> oneHotEncode(int index, int size) {
+    std::vector<double> one_hot(size, 0.0);
+    one_hot[index] = 1.0;
+    return one_hot;
+}
+
+// auto sigmoid = [](const std::vector<double> &x) -> std::vector<double> 
+// {
 //     std::vector<double> result(x.size());
-//     for (int i = 0; i < x.size(); i++) {
+//     for (int i = 0; i < x.size(); i++) 
+//     {
 //         result[i] = 1.0 / (1.0 + exp(-x[i]));
 //     }
 //     return result;
-// }
+// };
 
-// Softmax activation function
-// std::vector<double> softmax(const std::vector<double> &x) {
+// auto softmax = [](const std::vector<double> &x) -> std::vector<double>
+// {
 //     std::vector<double> result(x.size());
 //     double sum = 0.0;
-//     for (int i = 0; i < x.size(); i++) {
+//     for (int i = 0; i < x.size(); i++)
+//     {
 //         result[i] = exp(x[i]);
 //         sum += result[i];
 //     }
-//     for (int i = 0; i < x.size(); i++) {
+//     for (int i = 0; i < x.size(); i++)
+//     {
 //         result[i] /= sum;
 //     }
 //     return result;
-// }
+// };
 
-// std::vector<double> compute_layer(const std::vector<double> &inputs, const std::vector<std::vector<double>> &weights, const std::vector<double> &biases)
+// auto oneHotEncode = [](int index, int size) {
+//     std::vector<double> one_hot(size, 0);
+//     one_hot[index] = 1.0;
+//     return one_hot;
+// };
+
+// auto compute_layer = [](const std::vector<double> &inputs, const std::vector<std::vector<double>> &weights, const std::vector<double> &biases)
 // {
 //     std::vector<double> outputs(biases.size());
 
@@ -132,16 +139,7 @@ std::vector<double> mlp(std::vector<double>inputs)
 //         outputs[i] = dot_product;
 //     }
 //     return outputs;
-// }
-
-    // One hot encoding function
-    // std::vector<double> oneHotEncode(int index, int size) {
-    //     std::vector<double> one_hot(size, 0.0);
-    //     one_hot[index] = 1.0;
-    //     return one_hot;
-    // }
-
-
+// };
 
 
 
--- mlp.h
+++ mlp.h
@@ -8,6 +8,10 @@
 
 
 std::vector<double> mlp(std::vector<double>inputs);
-  
+std::vector<double> sigmoid(const std::vector<double> &x);
+std::vector<double> softmax(const std::vector<double> &x);
+std::vector<double> compute_layer(const std::vector<double> &inputs, const std::vector<std::vector<double>> &weights, const std::vector<double> &biases);
+std::vector<double> oneHotEncode(int index, int size);
+   
 
 #endif
