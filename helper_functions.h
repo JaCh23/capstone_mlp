73be0f84054f1c652564e4c1ce88952c0577b3ed
jared <jach2355@gmail.com>
2023-02-13 19:03:21 +0800

    weights biases refactor

--- helper_functions.h
+++ helper_functions.h
@@ -6,9 +6,9 @@
 #include <string>
 
 bool compareDataRow(const std::vector<double>& dataRow, const std::vector<double>& expectedDataRow);
-std::vector<std::vector<double>> arrayToVector(const double inputArray, int ROW_SIZE, int COL_SIZE);
+std::vector<std::vector<double> > arrayToVector(const double inputArray, int ROW_SIZE, int COL_SIZE);
 std::vector<double> read_file_arr(const std::string &filename);
 void print_vector(std::vector<double> v);
-std::vector<std::vector<double>> create_2d_vector_from_1d(const std::vector<double>& input, int num_cols);
+std::vector<std::vector<double> > create_2d_vector_from_1d(const std::vector<double>& input, int num_cols);
 
 #endif
