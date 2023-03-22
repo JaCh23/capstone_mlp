906f98c6d91e8b2d5619b43f43f3985cc4b18e72
jared <jach2355@gmail.com>
2023-03-22 15:47:06 +0800

    pca mlp circular buffer fix v2.3

--- ultra.py
+++ ultra.py
@@ -4,20 +4,16 @@ import traceback
 import random
 
 import time
-import csv
 import numpy as np
 import pandas as pd
-import pywt 
-import scipy.signal as sig
-from scipy import signal, stats
-from scipy.stats import entropy, kurtosis, skew
+from scipy import stats
 import joblib
 import json
 # import pynq
 # from pynq import Overlay
 
 
-class Training(threading.Thread):
+class AIModel(threading.Thread):
     def __init__(self):
         super().__init__()
 
@@ -27,11 +23,11 @@ class Training(threading.Thread):
         self.columns = ['gx', 'gy', 'gz', 'accX', 'accY', 'accZ']
 
         self.factors = ['mean', 'std', 'variance', 'range', 'peak_to_peak_amplitude',
-                    'mad', 'root_mean_square', 'interquartile_range', 'percentile_75',
-                   'energy']
+                        'mad', 'root_mean_square', 'interquartile_range', 'percentile_75',
+                        'energy']
 
         self.num_groups = 8
-        self.headers = [f'grp_{i+1}_{column}_{factor}' for i in range(self.num_groups)
+        self.headers = [f'grp_{i + 1}_{column}_{factor}' for i in range(self.num_groups)
                         for column in self.columns for factor in self.factors]
 
         self.headers.extend(['action'])
@@ -51,6 +47,7 @@ class Training(threading.Thread):
         self.pca_eigvecs_list = data['pca_eigvecs_list']
 
         self.pca_eigvecs_transposed = [list(row) for row in zip(*self.pca_eigvecs_list)]
+        
         # PYNQ overlay
         # self.overlay = Overlay("pca_mlp_1.bit")
         # self.dma = self.overlay.axi_dma_0
@@ -65,29 +62,29 @@ class Training(threading.Thread):
             pass
 
     def generate_simulated_data(self):
-        gx = random.uniform(-9, 9) # TODO - assumption: gyro x,y,z change btwn -9 to 9
+        gx = random.uniform(-9, 9)  # TODO - assumption: gyro x,y,z change btwn -9 to 9
         gy = random.uniform(-9, 9)
         gz = random.uniform(-9, 9)
         accX = random.uniform(-9, 9)
         accY = random.uniform(-9, 9)
         accZ = random.uniform(-9, 9)
         return [gx, gy, gz, accX, accY, accZ]
-    
+
     # simulate game movement with noise and action
     def generate_simulated_wave(self):
 
         # base noise 10s long -> 20Hz*10 = 200 samples
-        t = np.linspace(0, 5, 200) # Define the time range
-        x1 = 0.2 * np.sin(t) + 0.2 * np.random.randn(200) 
-        x1[(x1 > -1) & (x1 < 1)] = 0.0 # TODO - sensor noise within margin of error auto remove
-        
+        t = np.linspace(0, 5, 200)  # Define the time range
+        x1 = 0.2 * np.sin(t) + 0.2 * np.random.randn(200)
+        x1[(x1 > -1) & (x1 < 1)] = 0.0  # TODO - sensor noise within margin of error auto remove
+
         # movement motion
         period = 2  # seconds
         amplitude = 5
-        t = np.linspace(0, 2, int(2 / 0.05)) # Define the time range
-        x2 = amplitude * np.sin(2 * np.pi * t / period)[:40] # Compute the sine wave for only one cycle
+        t = np.linspace(0, 2, int(2 / 0.05))  # Define the time range
+        x2 = amplitude * np.sin(2 * np.pi * t / period)[:40]  # Compute the sine wave for only one cycle
 
-        x = x1 
+        x = x1
         # Add to the 40th-80th elements
         x[20:60] += x2
         x[80:120] += x2
@@ -106,13 +103,14 @@ class Training(threading.Thread):
         root_mean_square = np.sqrt(np.mean(np.square(data)))
         interquartile_range = stats.iqr(data)
         percentile_75 = np.percentile(data, 75)
-        energy = np.sum(data**2)
-        
+        energy = np.sum(data ** 2)
+
         output_array = np.empty((1, 10))
-        output_array[0] = [mean, std, variance, range, peak_to_peak_amplitude, mad, root_mean_square, interquartile_range, percentile_75, energy]                 
+        output_array[0] = [mean, std, variance, range, peak_to_peak_amplitude, mad, root_mean_square,
+                           interquartile_range, percentile_75, energy]
 
         return output_array
-    
+
     def preprocess_dataset(self, arr):
         processed_data = []
 
@@ -133,10 +131,10 @@ class Training(threading.Thread):
             start_idx = i * group_size
             end_idx = start_idx + group_size
             group = arr[start_idx:end_idx, :]
-            
+
             group_data = []
             for column in range(arr.shape[1]):
-                column_data = group[:,column]
+                column_data = group[:, column]
                 column_data = column_data.reshape(1, -1)
 
                 temp_processed = self.preprocess_data(column_data)
@@ -148,10 +146,10 @@ class Training(threading.Thread):
 
         processed_data_arr = np.concatenate(processed_data)
 
-        print(f"len processed_data_arr={len(processed_data_arr)}\n")
+        # print(f"len processed_data_arr={len(processed_data_arr)}\n")
 
         return processed_data_arr
-    
+
     def PCA_MLP(self, data):
         start_time = time.time()
 
@@ -173,7 +171,6 @@ class Training(threading.Thread):
         print(f"MLP time taken so far output: {time.time() - start_time}")
 
         return self.out_buffer
-    
 
     def instantMLP(self, data):
         # Load the model from file and preproessing
@@ -184,7 +181,7 @@ class Training(threading.Thread):
         # mlp = joblib.load('/home/xilinx/mlp_model.joblib')
 
         # sample data for sanity check
-        test_input = np.array([0.1, 0.2, 0.3, 0.4] * 120).reshape(1,-1)
+        test_input = np.array([0.1, 0.2, 0.3, 0.4] * 120).reshape(1, -1)
 
         # Scaler
         # test_input_rescaled = (data - self.mean) / np.sqrt(self.variance) # TODO - use this for real data
@@ -195,16 +192,7 @@ class Training(threading.Thread):
         test_input_math_pca = np.dot(test_input_rescaled, self.pca_eigvecs_transposed)
         print(f"test_input_math_pca: {test_input_math_pca}\n")
 
-        arr = np.array([-9.20434773, -4.93421279, -0.7165668, -5.35652778, 1.16597442, 0.83953718,
-                2.46925983, 0.55131264, -0.1671036, 0.82080829, -1.87265269, 3.34199444,
-                0.09530707, -3.77394007, 1.68183889, 1.97630386, 1.48839111, -3.00986825,
-                4.13786954, 1.46723819, 8.08842927, 10.94846901, 2.22280215, -1.85681443,
-                4.47327707, 3.15918201, -0.77879694, -0.11557772, 0.21580221, -2.62405631,
-                -3.42924226, -7.01213438, 7.75544419, -3.72408571, 3.46613566])
-
-        assert np.allclose(test_input_math_pca, arr)
-
-        # MLP
+        # MLP - TODO PYNQ Overlay
         # predicted_labels = self.PCA_MLP(test_input_math_pca) # return 1x4 softmax array
         # print(f"MLP pynq overlay predicted: {predicted_labels} \n")
         # np_output = np.array(predicted_labels)
@@ -216,12 +204,11 @@ class Training(threading.Thread):
         # print(f"largest index: {largest_index} \n")
         # print(f"MLP overlay predicted: {predicted_label} \n")
 
-        predicted_label = mlp.predict(test_input_math_pca.reshape(1, -1)) 
+        # MLP - LIB Overlay
+        predicted_label = mlp.predict(test_input_math_pca.reshape(1, -1))
         print(f"MLP lib overlay predicted: {predicted_label} \n")
 
-        # output is a single char
         return predicted_label
-
 
     def close_connection(self):
         self.shutdown.set()
@@ -231,12 +218,11 @@ class Training(threading.Thread):
     def run(self):
         # live integration loop
         window_size = 11
-        threshold_factor = 2
+        threshold_factor = 3
 
         buffer_size = 500
         buffer = np.zeros((buffer_size, len(self.columns)))
         # Define the window size and threshold factor
-        
 
         # Define N units for flagging movement, 20Hz -> 2s = 40 samples
         N = 40
@@ -252,11 +238,19 @@ class Training(threading.Thread):
 
         # while not self.shutdown.is_set():
         while True:
+            if 1 == 1:
+            # if ai_queue:
+
+                # Live data
+                # data = np.array(ai_queue.popleft())
+                # data[-3:] = [x/100.0 for x in data[-3:]]
+
+                # # Simulated data
                 data = self.generate_simulated_data()
-                self.sleep(0.05)
-                print("Data: ")
-                print(" ".join([f"{x:.8g}" for x in data]))
-                print("\n")
+                # self.sleep(0.05)
+                # print("Data: ")
+                # print(" ".join([f"{x:.3f}" for x in data]))
+                # print("\n")
 
                 # Append new data
                 buffer[buffer_index] = data
@@ -265,35 +259,30 @@ class Training(threading.Thread):
                 buffer_index = (buffer_index + 1) % buffer_size
 
                 # Compute absolute acceleration values
-                # x[buffer_index] = np.abs(data[3:6])  # abs of accX, accY, accZ
-                x[buffer_index] = wave[i] # abs of accX, accY, accZ
+                # x[buffer_index] = np.abs(np.sum(np.square(data[3:6])))  # abs of accX, accY, accZ
+                x[buffer_index] = wave[i]  # abs of accX, accY, accZ
 
                 i += 1
                 if i >= len(wave):
                     i = 0
 
                 # Compute moving window median
-                if buffer_index < window_size:
-                    filtered[buffer_index] = 0
-                else:
-                    filtered[buffer_index] = np.median(x[buffer_index-window_size+1:buffer_index+1], axis=0)
+                filtered[buffer_index] = np.median(x[buffer_index - window_size + 1:buffer_index + 1], axis=0)
 
                 # Compute threshold using past median data, threshold = mean + k * std
-                if buffer_index < window_size:
-                    threshold[buffer_index] = 0
-                else:
-                    past_filtered = filtered[buffer_index-window_size+1:buffer_index+1]
-                    threshold[buffer_index] = np.mean(past_filtered, axis=0) + (threshold_factor * np.std(past_filtered, axis=0))
+                past_filtered = filtered[buffer_index - window_size + 1:buffer_index + 1]
+                threshold[buffer_index] = np.mean(past_filtered, axis=0) + (
+                                threshold_factor * np.std(past_filtered, axis=0))
 
                 # Identify movement
-                if buffer_index >= window_size and np.all(filtered[buffer_index] > threshold[buffer_index]) and buffer_index - last_movement_time >= N:
+                if filtered[buffer_index] > threshold[buffer_index]:
                     last_movement_time = buffer_index  # update last movement time
                     print(f"Movement detected at sample {buffer_index}")
 
                 # if N samples from last movement time have been accumulated, preprocess and feed into neural network
-                if (buffer_index - last_movement_time) % buffer_size == N-1:
-                    # extract movement data
-                    start = (last_movement_time + 1) % buffer_size
+                if (last_movement_time != -N) and (buffer_index - last_movement_time + 1) % buffer_size == N:
+                    start = last_movement_time
+                    # +1 needed for python syntax, eg we want [1,40] but syntax is [1,41]
                     end = (buffer_index + 1) % buffer_size
                     if end <= start:
                         movement_data = np.concatenate((buffer[start:, :], buffer[:end, :]), axis=0)
@@ -304,12 +293,12 @@ class Training(threading.Thread):
                     print(f"Processing movement detected from sample {start} to {end}")
 
                     # perform data preprocessing
-                    preprocessed_data = self.preprocess_dataset(movement_data)
-            
+                    preprocessed_data = self.preprocess_dataset(movement_data) # multithread
+
                     # feed preprocessed data into neural network
-                    predicted_label = self.instantMLP(preprocessed_data)
-                    
-                    print(f"output from MLP: \n {predicted_label} \n") # print output of MLP
+                    predicted_label = self.instantMLP(preprocessed_data) # multithread
+
+                    print(f"output from MLP: \n {predicted_label} \n")  # print output of MLP
 
 
             # except Exception as _:
@@ -321,6 +310,6 @@ class Training(threading.Thread):
 if __name__ == '__main__':
     # AI Model
     print("Starting AI Model Thread")
-    ai_model = Training()
+    ai_model = AIModel()
     ai_model.start()
     print('--------------------------------------------')
